695e49716c516036bdf94b2340282181d3e2623f
AllanRicksr <allanrickrodrigues@hotmail.com>
2022-05-04 16:13:30 -0400

    adcinando funcoes de movimento e disparo

---
+++ script.js
@@ -0,0 +1,31 @@
+const yourShip = document.querySelector('.player-shooter');
+const playArea = document.querySelector('#main-play-game');
+
+
+//funcao movimento e disparo
+function flyShip(event) {
+    if (event.key === 'ArrowUp') {
+        event.preventDefault();
+        moveUp();
+    } else if (event.key === 'ArrowDown') {
+        event.preventDefault();
+        moveDown();
+    } else if (event.key === " ") {
+        event.preventDefault();
+        fireLaser();
+    }
+}
+
+//função de subir
+function moveUp() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if (topPosition === "0px") {
+        return;
+    } else {
+        let position = parseInt(topPosition);
+        position -= 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
+
+//função de descer
